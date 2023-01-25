b8bec5ed6cc3972589577b16a453a3f4ce8f7ba2
Idan Fischer <Idanfischer5@gmail.com>
2023-01-25 11:15:22 -0500

    added timestamp and fixed the get method

--- src/restaurants.js
+++ src/restaurants.js
@@ -1,13 +1,13 @@
-import dbConnect from "./dbConnect.js";
+import { FieldValue } from "firebase-admin/firestore"
+import dbConnect from "./dbConnect.js"
 const collectionName = "restaurants"
 
 // Get
 export async function getAllRestaurants(req, res) {
     const db = dbConnect()
-    const collection = db.collection(collectionName).get()
-    const restaurants = collection.docs.map( doc => { // element works too
-        return rest = doc.data  // rest isnt declared? but it works
-    })
+    const collection = await db.collection(collectionName)
+        .orderBy("createdAt", "desc").get()
+    const restaurants = collection.docs.map( doc => ({...doc.data(), restId: doc.id})) // element works too
     res.send(restaurants)
 }
 
@@ -16,7 +16,7 @@ export async function getRestaurantById(req, res) {
     const db = dbConnect()
     // const restId = req.params.restId
     const { restId } = req.params //.restId is no longer needs, the long way is no destructing
-    const doc = db.collection(collectionName).doc(restId).get()
+    const doc = await db.collection(collectionName).doc(restId).get()
     const rest = doc.data()
 
     res.send('Got Restaurant: ' + rest)
@@ -25,8 +25,9 @@ export async function getRestaurantById(req, res) {
 // Create
 export async function createRestaurant(req, res) {
     const db = dbConnect()
-    const newRestaurant = req.body
-    res.status(201).send('Added Restaurat')
+    let newRestaurant = req.body
+    // add a timestamp to the new restaurant
+    newRestaurant.createdAt = FieldValue.serverTimestamp()
     await db.collection(collectionName).add(newRestaurant)
     res.status(201).send('Added Restaurant')
 }
